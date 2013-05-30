868b50c9cc183ed917d411aae5356db578c5f0be
Renjith Pillai <rpillai@atlassian.com>
2013-05-30 19:52:28 +1000

    multiple handler resolved

--- src/main/resources/js/threaded-comments.js
+++ src/main/resources/js/threaded-comments.js
@@ -1,59 +1,59 @@
 function AddCommentButtons() {
     var loggedInUser = AJS.$('input[title="loggedInUser"]').val();
     var issueID = JIRA.Issue.getIssueId();
-    var iscommentallowed = AJS.$('#issue-comment-add-submit');
+    //var iscommentallowed = AJS.$('#issue-comment-add-submit');
 
-    console.log("loggedInUser - " + loggedInUser);
-    console.log("issueID - " + issueID);
-    console.log("iscommentallowed - " + iscommentallowed);
+    ///console.log("loggedInUser - " + loggedInUser);
+    console.log("AddCommentButtons called - " + issueID);
 
     AJS.$('div[id|=comment][id!=comment-wiki-edit]').each(function () {
         var commentId = AJS.$(this).attr('id').split('-')[1];
-        console.log("commentId - " + commentId);
 
         var commentBlock = AJS.$(this).children()[0];
         if (AJS.$(commentBlock).find('.commentreply').length == 0) {
+            console.log("Adding Reply block and handler for commentId - " + commentId);
             AJS.$(commentBlock).append(AJS.$('<a class="commentreply" href="#">Reply</a>'));
             AJS.$(commentBlock).append(AJS.$('<div class="commentreplyarea"><textarea class="textcommentreply"/>' +
                 '<ul class="ops"><li><a data="' + commentId + '" class="aui-button replycommentbutton">Add Reply</a></li><li><a href="#" data="' +
                 commentId + '" class="aui-button aui-button-link cancel replycommentcancel">Cancel</a></li></ul>' +
                 '</div>'));
-        }
-    });
-    AJS.$('.commentreply').click(function () {
-        event.preventDefault();
-        AJS.$(this).next().toggle();
-    });
-    AJS.$('.replycommentbutton').click(function () {
-        var newComment = AJS.$(this).parent().parent().parent().children("textarea").val();
-        if(newComment.length == 0 ) {
-            console.log("empty input");
-            return;
-        }
-        //AJS.$(this).parent().parent().parent().children("textarea").attr('readonly','readonly');
-        //AJS.$(this).parent().parent().parent().children("textarea").val('');
-        //AJS.$(this).parent().parent().parent().children("textarea").addClass('disabledtext');
 
-        console.log("new comment " + newComment);
-        var encoded = AJS.$('<div/>').text(newComment).html();
-        console.log("new comment " + encoded);
-        var data1 = '{"commentbody":"' + encoded + '","parentcommentid":"' + AJS.$(this).attr('data') + '","issueid":' + issueID + '}';
-        console.log("new data " + data1);
-        AJS.$.ajax({
-            url:AJS.contextPath() + "/rest/handlecomments/latest/addcomment",
-            data:data1,
-            type:"POST",
-            contentType:"application/json"
-        }).done(function (data) {
-            console.log("New comment added :");
-            JIRA.trigger(JIRA.Events.REFRESH_ISSUE_PAGE, [JIRA.Issue.getIssueId()]);
-        });
-        JIRA.trigger(JIRA.Events.REFRESH_ISSUE_PAGE, [JIRA.Issue.getIssueId()]);
+            AJS.$(this).find('.commentreply').click(function () {
+                event.preventDefault();
+                AJS.$(this).next().toggle();
+            });
+            AJS.$(this).find('.replycommentbutton').click(function () {
+                var newComment = AJS.$(this).parent().parent().parent().children("textarea").val();
+                if (newComment.length == 0) {
+                    console.log("empty input");
+                    return;
+                }
+                //AJS.$(this).parent().parent().parent().children("textarea").attr('readonly','readonly');
+                //AJS.$(this).parent().parent().parent().children("textarea").val('');
+                //AJS.$(this).parent().parent().parent().children("textarea").addClass('disabledtext');
+
+                //console.log("new comment " + newComment);
+                var encoded = AJS.$('<div/>').text(newComment).html();
+                //console.log("new comment " + encoded);
+                var data1 = '{"commentbody":"' + encoded + '","parentcommentid":"' + AJS.$(this).attr('data') + '","issueid":' + issueID + '}';
+                //console.log("new data " + data1);
+                AJS.$.ajax({
+                    url: AJS.contextPath() + "/rest/handlecomments/latest/addcomment",
+                    data: data1,
+                    type: "POST",
+                    contentType: "application/json"
+                }).done(function (data) {
+                        console.log("New comment added :");
+                        JIRA.trigger(JIRA.Events.REFRESH_ISSUE_PAGE, [JIRA.Issue.getIssueId()]);
+                    });
+                JIRA.trigger(JIRA.Events.REFRESH_ISSUE_PAGE, [JIRA.Issue.getIssueId()]);
+            });
+            AJS.$(this).find('.replycommentcancel').click(function () {
+                event.preventDefault();
+                AJS.$(this).parent().parent().parent().toggle();
+            });
+        }
     });
-    AJS.$('.replycommentcancel').click(function () {
-        event.preventDefault();
-        AJS.$(this).parent().parent().parent().toggle();
-    })
 }
 
 AJS.$('document').ready(function () {
