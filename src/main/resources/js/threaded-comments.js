a14519dfd2ad43b5a147a0f62771b81f61e3d536
Renjith Pillai <rpillai@atlassian.com>
2013-05-30 17:11:01 +1000

    Direct usage of JIRA comment block. Still doubtful.

--- src/main/resources/js/threaded-comments.js
+++ src/main/resources/js/threaded-comments.js
@@ -10,22 +10,25 @@ function AddCommentButtons() {
 
     AJS.$('div[id|=comment][id!=comment-wiki-edit]').each(function () {
         var commentId = AJS.$(this).attr('id').split('-')[1];
-        //var commentUser = AJS.$(this).find('.action-details a').attr("rel");
-        //console.log("commentUser - " + commentUser);
         console.log("commentId - " + commentId);
+        var idextra = "-" + commentId;
 
-        var commentBlock = AJS.$(this).children()[0];
-        AJS.$(commentBlock).append(AJS.$('<a class="commentreply" href="#">Reply</a>'));
-        AJS.$(commentBlock).append(AJS.$('<div class="commentreplyarea"><textarea class="textcommentreply"/>' +
-            '<ul class="ops"><li><a href="#" data="' + commentId + '" class="first last button replycommentbutton">Add Reply</a></li></ul>' +
-            '</div>'));
+        var replyButton = AJS.$('#addcommentmodule').clone(true,true);
+        replyButton.attr("id", replyButton.attr("id") + idextra);
+        replyButton.find('#footer-comment-button').removeClass("aui-button");
+        replyButton.find('#footer-comment-button').addClass("commentreply");
+        replyButton.find('#footer-comment-button').children("span:first").remove();
+        replyButton.find('#footer-comment-button').children("span:first").text("Reply");
+        AJS.$(this).append(replyButton);
+
     });
     AJS.$('.commentreply').click(function () {
         event.preventDefault();
         AJS.$(this).next().toggle();
     });
     AJS.$('.replycommentbutton').click(function () {
-        var newComment = AJS.$(this).previousSibling().val();
+        AJS.$(this).attr('readonly','readonly');
+        var newComment = AJS.$(this).previous().val();
         var encoded = $('<div/>').text(newComment).html();
         var data = '{"commentbody":"' + encoded + '","parentcommentid":"' + AJS.$(this).attr('data') + '","issueid":' + issueID + '}';
         AJS.$.ajax({
@@ -35,13 +38,14 @@ function AddCommentButtons() {
             contentType: "application/json",
             success: function (json) {
                 console.log("New comment added :");
+                location.reload();
             }
         });
     });
 }
 
 AJS.$('document').ready(function () {
-    //AddCommentButtons();
+    AddCommentButtons();
     JIRA.ViewIssueTabs.onTabReady(function(){
         AddCommentButtons();
     });
